21f9b19b12a256ed6525092ada04e2e926eebc41
kmskhn <khnkms@gmail.com>
2025-12-27 17:55:53 +0530

    learning SSG in pages router

--- Next.js/my-next12-app/pages/user/[id].jsx
+++ Next.js/my-next12-app/pages/user/[id].jsx
@@ -1,27 +1,127 @@
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+// APPROACH 1 --> CLIENT SIDE RENDERING
 
-const SingleUser = () => {
-  const router = useRouter();
-  const { id } = router.query;
+// import { useRouter } from "next/router";
+// import React, { useEffect, useState } from "react";
 
-  useEffect(() => {
-    console.log("inside useEffect");
+// const SingleUser = () => {
+//   const router = useRouter();
+//   const { id } = router.query;
+//   const [data, setData] = useState({})
 
-    async function fetchUserData() {
-      console.log("inside function");
-      console.log("id", id);
+//   useEffect(() => {
 
-      if (!router.isReady) return;
-      const res = await fetch(`https://dummyjson.com/products/${id}`);
-      const data = await res.json();
-      console.log(data);
-    }
+//     async function fetchUserData() {
 
-    fetchUserData();
-  }, [router.isReady, id]);
+//       if (!router.isReady) return;
+//       const res = await fetch(`https://dummyjson.com/products/${id}`);
+//       const data = await res.json();
+//       setData(data)
+//     }
 
-  return <div>I am a single user with ID: {id}</div>;
+//     fetchUserData();
+//   }, [router.isReady, id]);
+
+//   if(!data || Object.keys(data).length === 0) return <p>Loading...</p>
+
+//   return (
+//     <>
+//       <header className="bg-red-300 p-5">
+//         <p>Home</p>
+//         <p>About</p>
+//         <p>Contact</p>
+//       </header>
+//       <h1>{data?.title}</h1>
+//       <p>{data?.description}</p>
+//       <img src={data?.thumbnail} alt={data?.title} />
+//     </>
+//   )
+// };
+
+// export default SingleUser;
+
+
+// APPROACH 2 --> SERVER SIDE RENDERING
+// const SingleUser = ({product, id}) => {
+  
+//   return (
+//     <>
+//       <header className="bg-red-300 p-5">
+//         <p>Home</p>
+//         <p>About</p>
+//         <p>Contact</p>
+//       </header>
+//       <h1>{product?.title}</h1>
+//       <p>{product?.description}</p>
+//       <img src={product?.thumbnail} alt={product?.title} />
+//     </>
+//   );
+// };
+
+// export default SingleUser;
+
+// export const getServerSideProps = async (context) => {
+//   const id = context?.params?.id
+//   const res = await fetch(`https://dummyjson.com/products/${id}`);
+//   const data = await res.json();
+//   return {
+//     props: {
+//       product: data,
+//       id: id
+//     }
+//   }
+// }
+
+
+// APPROACH 3 --> SSG
+
+const SingleUser = ({product, id}) => {
+  return (
+    <>
+      <header className="bg-red-300 p-5">
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+      </header>
+      <h1>{product?.title}</h1>
+      <p>{product?.description}</p>
+      <img src={product?.thumbnail} alt={product?.title} />
+    </>
+  );
 };
 
 export default SingleUser;
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://dummyjson.com/products/`);
+    const data = await res.json();
+    const products = data.products;
+
+    const paths = products.map((product) => ({
+      params: {id: product.id.toString()}
+    }))
+
+    return {
+      paths,
+      fallback: "blocking"
+    }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context?.params?.id
+  const res = await fetch(`https://dummyjson.com/products/${id}`);
+  const data = await res.json();
+  console.log("here!!");
+
+  if(!data || !data.id){
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      product: data,
+      id: id
+    }
+  }
+}
